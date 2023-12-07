b46926cf33c3907ba20d600bdfbcafafcbde37b4
Matteo <herbert71@hotmail.it>
2023-12-07 16:05:21 +0100

    Day 03, first solution - does not work

--- src/day03.zig
+++ src/day03.zig
@@ -8,16 +8,13 @@ const demo = @embedFile("input/" ++ name ++ "_demo.txt");
 
 test "day03 input processing" {
     const grid = Grid.init(demo);
-    try testing.expectEqual(grid.size, Vec{ .x = 10, .y = 10 });
+    try testing.expectEqual(grid.w, 10);
+    try testing.expectEqual(grid.h, 10);
 
-    try testing.expectEqual(grid.get(Vec{ .x = 2, .y = 0 }), '7');
-    try testing.expectEqual(grid.get(Vec{ .x = 9, .y = 4 }), '.');
-    try testing.expectEqual(grid.get(Vec{ .x = 0, .y = 9 }), '.');
-    try testing.expectEqual(grid.get(Vec{ .x = 9, .y = 9 }), '.');
-    try testing.expectEqual(grid.get(Vec{ .x = 3, .y = 9 }), '4');
+    try testing.expectEqualStrings(grid.row(1), "...*......");
 
-    try testing.expect(grid.hasSymbolAdjacent(Vec{ .x = 2, .y = 0 }));
-    try testing.expect(!grid.hasSymbolAdjacent(Vec{ .x = 1, .y = 0 }));
+    try testing.expect(grid.isPartNumber(2, 0));
+    try testing.expect(!grid.isPartNumber(1, 0));
 }
 
 test "day03 part1" {
@@ -32,8 +29,44 @@ test "day03 part2" {}
 
 fn part1(input: []const u8) u32 {
     const grid = Grid.init(input);
-    _ = grid;
-    return 0;
+    var sum: u32 = 0;
+
+    for (0..grid.h) |y| {
+        const row = grid.row(y);
+
+        var first_digit: ?usize = null;
+
+        for (0..grid.w, row) |x, cell| {
+            if (std.ascii.isDigit(cell)) {
+                first_digit = x;
+                break;
+            }
+        }
+
+        // If digit not found on current row, skip to next row
+        const x0 = first_digit orelse continue;
+
+        var value: u32 = 0;
+        var part_number = false;
+
+        for (x0..grid.w, row[x0..grid.w]) |x, cell| {
+            if (!part_number) part_number = grid.isPartNumber(x, y);
+
+            if (!std.ascii.isDigit(cell)) {
+                // Number terminated
+                break;
+            }
+
+            value = value * 10 + cell - '0';
+        }
+
+        if (part_number) {
+            std.log.debug("Value: {d}", .{value});
+            sum += value;
+        }
+    }
+
+    return sum;
 }
 
 fn part2(input: []const u8) u32 {
@@ -41,84 +74,64 @@ fn part2(input: []const u8) u32 {
     return 0;
 }
 
-const Vec = struct {
-    x: i32 = 0,
-    y: i32 = 0,
-};
-
-const Digit = struct {
-    pos: Vec = .{},
-    len: u32 = 0,
-    value: u32 = 0,
-};
-
-fn isSymbol(char: u8) bool {
-    return !std.ascii.isDigit(char) and char != '.';
-}
-
 const Grid = struct {
     input: []const u8,
-    size: Vec,
+    w: usize,
+    h: usize,
 
     const new_line = "\r\n";
 
     pub fn init(input: []const u8) Grid {
-        return .{ .input = input, .size = measure(input) };
-    }
-
-    pub fn hasSymbolAdjacent(grid: *Grid, pos: Vec) bool {
-        const offsets = [_]Vec{
-            .{ .x = 1, .y = 0 },
-            .{ .x = 1, .y = 1 },
-            .{ .x = 0, .y = 1 },
-            .{ .x = -1, .y = 1 },
-            .{ .x = -1, .y = 0 },
-            .{ .x = -1, .y = -1 },
-            .{ .x = 0, .y = -1 },
-            .{ .x = 1, .y = -1 },
-        };
-
-        for (offsets) |offset| {
-            const cursor = Vec{ .x = pos.x + offset.x, .y = pos.y + offset.y };
-            if (cursor.x < 0 or cursor.y < 0) continue;
-            if (cursor.x >= grid.size.x or cursor.y >= grid.size.y) continue;
-
-            if (isSymbol(grid.get(cursor))) return true;
-        }
-
-        return false;
-    }
-
-    pub fn get(grid: *Grid, pos: Vec) u8 {
-        assert(grid.size.x > 0 and grid.size.y > 0);
-        assert(pos.x >= 0 and pos.x < grid.size.x);
-        assert(pos.y >= 0 and pos.y < grid.size.y);
-
-        const nl: i32 = @intCast(new_line.len);
-        const offset: usize = @intCast(pos.x + pos.y * (grid.size.x + nl));
-        return grid.input[offset];
-    }
-
-    fn measure(input: []const u8) Vec {
         var start = std.mem.indexOf(u8, input, new_line) orelse unreachable;
 
-        var size = Vec{
-            .x = @intCast(start),
-            .y = 2, // Account for first (just scanned) and last line (not terminated)
+        var grid = Grid{
+            .input = input,
+            .w = start,
+            .h = 2, // Account for first (just scanned) and last line (not terminated)
         };
 
         while (true) {
             start += new_line.len;
 
             if (std.mem.indexOfPos(u8, input, start, new_line)) |pos| {
-                assert(pos - start == size.x);
+                assert(pos - start == grid.w);
                 start = pos;
-                size.y += 1;
+                grid.h += 1;
             } else break;
         }
 
-        assert(input[start..].len == size.x);
+        assert(input[start..].len == grid.w);
 
-        return size;
+        return grid;
+    }
+
+    pub fn isPartNumber(grid: Grid, x: usize, y: usize) bool {
+        const offsets = [_][2]i2{
+            .{ 1, 0 },  .{ 1, 1 },   .{ 0, 1 },  .{ -1, 1 }, //
+            .{ -1, 0 }, .{ -1, -1 }, .{ 0, -1 }, .{ 1, -1 },
+        };
+
+        inline for (offsets) |offset| {
+            const nx = @as(isize, @intCast(x)) + offset[0];
+            const ny = @as(isize, @intCast(y)) + offset[1];
+
+            if (nx >= 0 and ny >= 0 and nx < grid.w and ny < grid.h) {
+                const width: isize = @intCast(grid.w + new_line.len);
+                const cell = grid.input[@intCast(nx + ny * width)];
+
+                // If the ajacent cell is a symbol (excluding dots), the cell can be a part number
+                if (!std.ascii.isDigit(cell) and cell != '.') return true;
+            }
+        }
+
+        return false;
+    }
+
+    pub fn row(grid: Grid, n: usize) []const u8 {
+        assert(grid.w > 0 and grid.h > 0);
+        assert(n >= 0 and n < grid.h);
+
+        const offset = n * (grid.w + new_line.len);
+        return grid.input[offset..][0..grid.w];
     }
 };
